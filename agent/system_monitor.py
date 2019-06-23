f13b0c8ebd4eaaa6f2a1166ccb53603bba9bdccf
r-dog <jake.kgrogan@gmail.com>
2019-06-23 18:10:36 +0100

    Added name parameter to CPU_WIDE_PERCENTAGE

--- agent/system_monitor.py
+++ agent/system_monitor.py
@@ -6,12 +6,12 @@ class SystemMonitor(object):
     def pub_CPU_wide_metric(self):
         cpu_percent = psutil.cpu_percent(interval=0.1)
         with open("../logs/cpu_wide_perc.log", "a+") as f:
-            f.write("{}-{}: {}\n".format("CPU_utilization", time.time(), cpu_percent))
+            f.write("{}-{}: {}\n".format("CPU_UTILIZATION", time.time(), cpu_percent))
     
     def pub_CPU_per_core_metric(self):
         cpu_percent = psutil.cpu_percent(interval=0.1, percpu=True)
         with open("../logs/cpu_per_core_perc.log", "a+") as f:
-            f.write("{}: {}\n".format(time.time(), cpu_percent))
+            f.write("{}-{}: {}\n".format("CPU_PER_CORE_PERCENTAGE",time.time(), cpu_percent))
 
     def pub_CPU_stats(self):
         cpu_stats = psutil.cpu_stats()
@@ -19,7 +19,7 @@ class SystemMonitor(object):
         with open("../logs/cpu_stats.log", "a+") as f:
             for name in cpu_stats._fields:
                 value = getattr(cpu_stats, name)
-                f.write("{}-{}: {}\n".format(name, curr_time, value))
+                f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
 
 
     def pub_CPU_wide_freq(self):
@@ -28,7 +28,7 @@ class SystemMonitor(object):
         with open("../logs/cpu_freq.log", "a+") as f:
             for name in cpu_freq._fields:
                 value = getattr(cpu_freq, name)
-                f.write("{}-{}: {}\n".format(name, curr_time, value))
+                f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
 
     def pub_virt_memory_stats(self):
         mem = psutil.virtual_memory()
@@ -36,7 +36,7 @@ class SystemMonitor(object):
         with open("../logs/virtual_memory_stats.log", "a+") as f:
             for name in mem._fields:
                 value = getattr(mem, name)
-                f.write("{}-{}: {}\n".format(name, curr_time, value))
+                f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
 
     # IMPLEMENT SWAP MEMORY STATISTICS
 
@@ -46,7 +46,7 @@ class SystemMonitor(object):
         with open("../logs/disk_usage.log", "a+") as f:
             for name in usage._fields:
                 value = getattr(usage, name)
-                f.write("{}-{}: {}\n".format(name, curr_time, value))
+                f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
 
     def pub_disk_wide_io_counters(self):
         io_counters = psutil.disk_io_counters()
@@ -54,7 +54,7 @@ class SystemMonitor(object):
         with open("../logs/disk_io_counters.log", "a+") as f:
             for name in io_counters._fields:
                 value = getattr(io_counters, name)
-                f.write("{}-{}: {}\n".format(name, curr_time, value))
+                f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
 
     def pub_net_io_counters(self):
         net_io_c = psutil.net_io_counters()
@@ -62,7 +62,7 @@ class SystemMonitor(object):
         with open("../logs/network_io_counters.log", "a+") as f:
             for name in net_io_c._fields:
                 value = getattr(net_io_c, name)
-                f.write("{}-{}: {}\n".format(name, curr_time, value))
+                f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
 
     def pub_core_temperature(self):
         sensor_temperature = psutil.sensors_temperatures()
@@ -78,7 +78,7 @@ class SystemMonitor(object):
                         else:
                             f.write("{}\n".format(value))
                     else:
-                        f.write("{}-{}: {}\n".format(name, curr_time, value))
+                        f.write("{}-{}: {}\n".format(name.upper(), curr_time, value))
     
     def pub_hw_fan_speed(self):
         fans = psutil.sensors_fans()
@@ -87,9 +87,9 @@ class SystemMonitor(object):
             for k, v in fans.items():
                 for fan in v:
                     if fan.label != "":
-                        f.write("{}-{}-{}: {}\n".format(k, fan.label, curr_time, fan.current))
+                        f.write("{}-{}: {}\n".format(fan.label.upper(), curr_time, fan.current))
                     else:
-                        f.write("{}-{}: {}\n".format(k, curr_time, fan.current))
+                        f.write("{}-{}: {}\n".format(k.upper(), curr_time, fan.current))
 
     def publish_all_metrics(self):
         self.pub_CPU_wide_metric()
